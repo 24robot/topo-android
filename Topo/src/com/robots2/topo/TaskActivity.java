979cca408a2e8c73eb54f2bd01091fea9ebd34d0
Anirudh S <anirudh24seven@gmail.com>
2014-04-17 11:07:47 +0530

    Added new activity for adding task
    Added spinners for primary tasks in the first activity, add-task button
    in the action bar clicking on which takes user to the new activity for
    adding tasks.

--- Topo/src/com/robots2/topo/TaskActivity.java
+++ Topo/src/com/robots2/topo/TaskActivity.java
@@ -1,60 +1,23 @@
 package com.robots2.topo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 
 public class TaskActivity extends ListActivity {
 
 	private TaskDataSource dataSource;
 	
-	private ProgressBar mProgressBar;
-	
-	private Spinner mColorSpinner;
-	
-	private String redPrimaryTask;
-	private String greenPrimaryTask;
-	private String bluePrimaryTask;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task);
-
-		mProgressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
-		mProgressBar.setVisibility(ProgressBar.GONE);
-		
-		mColorSpinner = (Spinner) findViewById(R.id.colorSpinner);
-		
-		List<String> colors = new ArrayList<String>();
-		
-		if (redPrimaryTask == null) {
-			colors.add("Red Primary Task");
-		}
-		
-		if (greenPrimaryTask == null) {
-			colors.add("Green Primary Task");
-		}
-		
-		if (bluePrimaryTask == null) {
-			colors.add("Blue Primary Task");
-		}
-		
-		if (!colors.isEmpty()) {
-			ArrayAdapter adapter = new ArrayAdapter(this, R.layout.spinner_item, colors);
-			mColorSpinner.setAdapter(adapter);
-			
-		}
 		
 		dataSource = new TaskDataSource(this);
 		dataSource.open();
@@ -63,42 +26,15 @@ public class TaskActivity extends ListActivity {
 		ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(this, R.layout.row_list_task, R.id.task_description, tasks);
 		setListAdapter(adapter);
 		
+		Spinner redPrimaryTaskSpinner = (Spinner) findViewById(R.id.red_primary_task_spinner);
+		Spinner greenPrimaryTaskSpinner = (Spinner) findViewById(R.id.green_primary_task_spinner);
+		Spinner bluePrimaryTaskSpinner = (Spinner) findViewById(R.id.blue_primary_task_spinner);
+		
+		redPrimaryTaskSpinner.setAdapter(adapter);
+		greenPrimaryTaskSpinner.setAdapter(adapter);
+		bluePrimaryTaskSpinner.setAdapter(adapter);
 	}
 	
-	public void onClick(View view) {
-		@SuppressWarnings("unchecked")
-		ArrayAdapter<Task> adapter = (ArrayAdapter<Task>) getListAdapter();
-		Task task = null;
-		
-		switch(view.getId()) {
-			case R.id.buttonAddTask:
-				EditText e = (EditText) findViewById(R.id.textNewTask);
-				String description = e.getText().toString();
-				
-				if (description != null) {
-					task = dataSource.addTask(description);
-					adapter.add(task);
-				}
-				
-				e.setText("");
-				
-				break;
-			
-			case R.id.task_description:
-				if (getListAdapter().getCount() > 0) {
-					ListView lv = getListView();
-					int position = lv.getPositionForView(view);
-					
-					task = (Task) getListAdapter().getItem(position);
-			        dataSource.deleteTask(task);;
-			        adapter.remove(task);
-				}
-				
-				break;
-		}
-		
-		adapter.notifyDataSetChanged();
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -109,8 +45,16 @@ public class TaskActivity extends ListActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch(id) {
+			case (R.id.action_add_task):
+				Intent addNewTaskIntent = new Intent(this, NewTaskActivity.class);
+				addNewTaskIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+				startActivity(addNewTaskIntent);
+				break;
+			case (R.id.action_settings):
+				break;
+			default:
+				break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
